7c9949a649604df9d6f06ec5550b678097a37d79
jdelgadou-POO <jdelgadou@unal.edu.co>
2024-11-06 21:00:41 -0500

    Prueba 8

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -9,17 +9,25 @@ public class Auto {
     int registro;
     static String cantidadCreados;
     public int cantidadAsientos(){
-        return asientos.length;
-    }
+        contado=0;
+        for (int i=0; i<asientos.length; ++i) {
+            if (asientos[i] instanceof Asiento) {
+                contador+=1;
+            }}
+        return contador;
+        }
     public String verificarIntegridad(){
         if (motor.registro!=registro){
             return "Las piezas no son originales";
         }
         for (int i=0; i<asientos.length; ++i) {
-            if (asientos[i].registro != registro) {
+            if (asientos[i] instanceof Asiento) {
+                if (asientos[i].registro != registro) {
                 return "Las piezas no son originales";
-            }
-        }
+            };
+            }}
+            
+        
             return "Auto original";
     
     }
